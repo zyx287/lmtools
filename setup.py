bb10bb42e4660b493a05031ffefa1044ac4c0e50
zyx287 <yuxiang.jeffrey.zhang@gmail.com>
2025-06-10 14:52:17 -0400

    Update: New version control

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup(
         name="lmtools",
-        version="0.1.0",
+        version="0.2.0",
         packages=find_packages(),  # No 'where' parameter needed
         install_requires=[
             "numpy",
